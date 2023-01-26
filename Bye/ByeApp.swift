51de01f7d234ba22ebe08feff9cc2dff754ba583
wooju <dnwnrnt@naver.com>
2023-01-26 15:17:12 +0900

    Initial Commit

---
+++ Bye/ByeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByeApp.swift
+//  Bye
+//
+//  Created by wooju on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ByeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
